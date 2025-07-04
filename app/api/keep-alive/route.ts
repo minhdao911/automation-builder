696287b7d3adef90cbf653df11b956fa6088cdf2
Minh Dao <daominh91197@gmail.com>
2025-07-04 10:51:41 +0300

    fix the cron job schedule to be more often

--- app/api/keep-alive/route.ts
+++ app/api/keep-alive/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const count = await db.user.count();
+  console.log(`Ping DB OK: ${count} 🤖`);
 
   return new NextResponse(`Ping DB OK: ${count} 🤖`, { status: 200 });
 }
